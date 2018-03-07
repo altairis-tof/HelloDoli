3e037cfa99941d5009cc9fe51860d19f0a70807f
Jonathan <mazal.jonathan@gmail.com>
2018-03-07 22:51:59 +0100

    Ajout template de configuration
    Ajout du fichier de configuration vierge.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+  $helloAssoAPIUrl = "https://api.helloasso.com/v3/";
+  $helloAssoUsername = "";
+  $helloAssoAPIPassword = "";
+
+  $db_username = "";
+  $db_password = "";
+  $db_name = "";
+  $db_type = "mysql";
+  $db_host = "localhost";
+
+  $dolibarrAPIUrl = "";
+  $dolibarrToken = "";
+
+?>
